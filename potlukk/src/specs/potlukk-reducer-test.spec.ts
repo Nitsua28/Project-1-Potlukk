05e01938de7ebdcc9a0befab600d7a2d45d8e2d6
Erich-D <33156265+Erich-D@users.noreply.github.com>
2023-02-13 12:07:01 -0700

    add tests

---
+++ potlukk/src/specs/potlukk-reducer-test.spec.ts
@@ -0,0 +1,26 @@
+import { LukkerUserInfo, lukkerUserReducer, LukkerUserState } from "../reducers/potlukk-reducer"
+
+const testState:LukkerUserState ={
+    currentUser: {
+        userId:0,
+        username:"",
+        fname:"",
+        lname:"",
+        allergies:[]
+    },
+    userList:[]
+}
+
+
+test("ADD user", ()=>{
+    const testUser: LukkerUserInfo = {
+        userId: 1502,
+        username: "joe65",
+        fname: "Joe",
+        lname: "Smith",
+        allergies: ["EGG","SOY"]
+    }
+
+    const nextState = lukkerUserReducer(testState,{type:"ADD_USER",payload:testUser});
+    expect(nextState.userList.length).toBe(1)
+})

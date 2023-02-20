089e72cfa25efc7011dd173f760ceb2af954ad77
Erich-D <33156265+Erich-D@users.noreply.github.com>
2023-02-19 18:23:30 -0700

    fix test

--- public/sign-in-test.tsx
+++ public/sign-in-test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event";
-import App from "../App"
+import React from "react";
+import App from "../src/App"
 
 test("signin page", async ()=>{
     render(<App/>)
